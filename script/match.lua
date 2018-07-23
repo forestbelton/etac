99900d771a7e45d54388200a324aac2e27ec5783
Forest Belton <forest@homolo.gy>
2018-07-23 02:29:08 -0700

    Fix unbounded variable in turn queue

--- script/match.lua
+++ script/match.lua
@@ -42,12 +42,12 @@ function Match:enqueue (entityId)
     }
 
     -- If there are other elements, insert increasing by time.
-    -- for i = 1, #self.queue do
-    --     #if time < self.queue[i].time then
-    --         table.insert(self.queue, i, entry)
-    --         return
-    --     end
-    -- end
+    for i = 1, #self.queue do
+        if entry.time < self.queue[i].time then
+            table.insert(self.queue, i, entry)
+            return
+        end
+    end
 
     self.queue[#self.queue + 1] = entry
 end
