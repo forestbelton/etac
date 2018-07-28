42b697cc16ef91d3be8fc88cb0a0ba878e189345
Forest Belton <forest@homolo.gy>
2018-07-28 12:35:41 -0700

    Document screen functions

--- include/ui/screen.h
+++ include/ui/screen.h
@@ -15,8 +15,25 @@
 
 #define OBJECT_COUNT 54 * 14
 
+/*
+ * Draw the game window.
+ * @param window An array of cells to render in the window. The array must
+ *               have a length of WINDOW_WIDTH * WINDOW_HEIGHT.
+ */
 void screen_draw_window(const struct tb_cell *window);
+
+/*
+ * Draw a game object in the window space.
+ * @param object The cell of the game object.
+ * @param x The x coordinate of the cell relative to the window.
+ * @param y The y-coordinate of the cell relative to the window.
+ */
 void screen_draw_object(struct tb_cell object, size_t x, size_t y);
+
+/*
+ * Draw the game action log.
+ * @param env The Lua environment containing the match object.
+ */
 void screen_draw_logs(lua_State *env);
 
 #endif
